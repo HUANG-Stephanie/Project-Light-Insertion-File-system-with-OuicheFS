2a09755d3593612ba55853b1a9dcf8a651ce3178
paul-jns <125988111+paul-jns@users.noreply.github.com>
2024-05-17 16:29:43 +0200

    Update installOuichefs.sh
    commentaires

--- installOuichefs.sh
+++ installOuichefs.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+#installe ouichefs et fait un premier make et met le module dans le dossier
+#share
+
+#Path a changer en fonction des prefs
 cd /tmp/PNL/
 
 git clone https://github.com/rgouicem/ouichefs.git
@@ -7,7 +11,9 @@ git clone https://github.com/rgouicem/ouichefs.git
 gcc -Wall -o mkfs.ouichefs mkfs-ouichefs.c
 
 cd ..
+
+#Path a changer en fonction des prefs
 make KERNELDIR=/tmp/PNL/linux-6.5.7
 
-#
+#Path a changer en fonction des prefs
 cp ouichefs.ko /users/nfs/Etu8/28634278/PNL/lkp-lap_02-src/src/scripts/share
